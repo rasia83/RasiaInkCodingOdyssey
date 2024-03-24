282b07da84b487914dbac719c00b02f407db462c
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2024-03-24 11:22:17 -0300

    add base as github repo

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/RasiaInkCodingOdyssey/',
   lang: 'en-US',
   title: "Java Journey by Rasia",
   description: "Coding the Path to Java Mastery",
